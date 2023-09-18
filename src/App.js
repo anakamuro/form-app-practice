a0ff85f310c99d7b4d7a1e8c9ab24bbe897e1b77
Akihiko Nakamura <anakamuro@earthlink.net>
2023-09-18 19:35:08 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react"
 
 function App() {
+  const [firstName, setFirstName] = useState('');
+  const [email, setEmail] = useState('');
+  const [people, setPeople] = useState([])
+
+  const removeItem = (id)=> {
+    let newPeople = people.filter((person) => person.id !== id)
+    setPeople(newPeople)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (firstName && email){
+      const person = {id: new Date().getTime().toString(), firstName, email};
+      setPeople((people) => {
+        return [...people, person];
+      })
+      setFirstName('');
+      setEmail('');
+    } else {
+      console.log('email values')
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <article>
+        <form className="form" onSubmit={handleSubmit}>
+          <div className="form-control">
+            <label htmlFor='firstName'>Name: </label>
+            <input 
+               type="text"
+               id='firstName'
+               name='firstName'
+               value={firstName}
+               onChange={(e) => setFirstName(e.target.value)}
+               />
+          </div>
+          <div className='form-control'>
+            <label htmlFor='email'>Email: </label>
+            <input type="text" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+          </div>
+          <button type="submit">add person</button>
+        </form>
+        {people.map((person, index) => {
+          const {id, firstName, email } = person;
+          return (
+            <div className='item' key={id}>
+            <h4>{firstName}</h4>
+            <p>{email}</p>
+            <button className="btn" onClick={() => removeItem(id)}>remove</button>
+            </div>
+          )
+        })}
+      </article>
     </div>
   );
 }
